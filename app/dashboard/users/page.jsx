26f4c5c3a1b110f93b1fd96ff288520f78888ee0
Halit Özken <halitozken@gmail.com>
2024-02-05 14:43:17 +0300

    users page completed

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -2,6 +2,7 @@ import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 
 const UsersPage = () => {
   return (
@@ -27,18 +28,37 @@ const UsersPage = () => {
           <tr>
             <td>
               <div className={styles.user}>
-                <Image 
-                src="/noavatar.png" 
-                alt="" 
-                width={40}
-                height={40}
-                className={styles.userImage}
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
                 />
+              </div>
+            </td>
+            <td>john@gmail.com</td>
+            <td>05.02.2024</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </Link>
               </div>
             </td>
           </tr>
         </tbody>
       </table>
+      <Pagination />
     </div>
   );
 };
