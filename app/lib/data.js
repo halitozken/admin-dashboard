eb55afdfc1f8d9772568eb032e492fb4b75e55cd
Halit Özken <halitozken@gmail.com>
2024-02-07 16:03:13 +0300

    search functionality

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,10 +1,12 @@
 import { User } from "./models";
 import { connectToDb } from "./utils";
 
-export const fetchUsers = async () => {
+export const fetchUsers = async (q) => {
+  const regex = new RegExp(q, "i");
+
   try {
     connectToDb();
-    const users = await User.find();
+    const users = await User.find({ username: { $regex: regex } });
     return users;
   } catch (error) {
     console.error(error);
--- app/lib/utils.js
+++ app/lib/utils.js
@@ -5,7 +5,7 @@ export const connectToDb = async () => {
 
   try {
     if (connection.isConnected) return;
-    await mongoose.connect(process.env.MONGO);
+    const db = await mongoose.connect(process.env.MONGO);
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.error(error);
