7ce7bb07f9949b170a690c9e10365030da4d77a0
Halit Ozken <halitozken@gmail.com>
2024-02-13 16:50:00 +0300

    form validate

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,4 +1,29 @@
-export const addUser = async () => {
+import { User } from "./models";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
+export const addUser = async (formData) => {
+  "use server";
   const { username, email, password, phone, address, isAdmin, isActive } =
     Object.fromEntries(formData);
+
+  try {
+    const newUser = new User({
+      username,
+      email,
+      password,
+      phone,
+      address,
+      isAdmin,
+      isActive,
+    });
+
+    await newUser.save();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create user!");
+  }
+
+  revalidatePath("/dashboard/users");
+  redirect("/dashboard/users");
 };
